692c77e73a034cfe735b5af68e97830c152977c1
umashravani24 <umashravani24@gmail.com>
2025-07-03 00:50:25 +0530

    test updated

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+# This program takes two inputs from user and shows as string,
 x=input()
 y=input()
 z=x+y
 print(z)
+#this program takes two inputs from user and adds them as integers,
+x1=int(input("Enter first number: "))
+y1=int(input("Enter second number: "))
+z1=x1+y1
+print(z1)   
