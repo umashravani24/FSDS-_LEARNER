3f0ab8f2cba20cbec7e85df0bade50f38debf4f4
umashravani24 <umashravani24@gmail.com>
2025-07-03 01:14:42 +0530

    srting input updated

---
+++ add.py
@@ -0,0 +1,8 @@
+# This program takes a string input and extracts specific characters,
+st1=input("enter a string:")[1]
+st2=input("enter a string:")[2:5]
+st3=input("enter a expression: ")
+res=eval(st3)
+print(st1)
+print(st2)
+print(res)
